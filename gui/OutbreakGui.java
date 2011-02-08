0be788de9dfee13328c77fa3719f54526edfecb7
fbielejec <fbielejec@gmail.com>
2011-02-08 15:28:55 +0100

    simple gui

--- gui/OutbreakGui.java
+++ gui/OutbreakGui.java
@@ -1,20 +1,26 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
 
 public class OutbreakGui {
 
@@ -24,6 +30,7 @@ public class OutbreakGui {
 	private JFrame Frame = new JFrame("TestlabOutbreak"); // create Frame
 
 	// Buttons with options
+	private JButton Open = new JButton("Open");
 	private JSeparator Separator = new JSeparator(JSeparator.VERTICAL);
 	private JButton Help = new JButton("Help");
 	private JButton Quit = new JButton("Quit");
@@ -31,16 +38,20 @@ public class OutbreakGui {
 	// Menubar
 	private JMenuBar mb = new JMenuBar();
 
-	// Icon
-	ImageIcon treeIcon = createImageIcon("/images/tree.png");
+	// Text fields
+	JTextField coordinatesNameParser = new JTextField(10);
 
 	/** Constructor for the GUI */
 	public OutbreakGui() {
 
+		coordinatesNameParser.setActionCommand("coordinatesNameParser");
+
 		// Set menubar
 		Frame.setJMenuBar(mb);
+		Frame.add(coordinatesNameParser);
 
 		// Build Menus
+		mb.add(Open);
 		mb.add(Separator);
 		mb.add(Help);
 		mb.add(Quit);
@@ -54,11 +65,24 @@ public class OutbreakGui {
 
 		// Add Menu listeners
 		Quit.addActionListener(new ListenMenuQuit());
+		Open.addActionListener(new ListenMenuOpen());
 	}
 
 	private class ListenMenuQuit implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			System.exit(0);
+		}
+	}
+
+	public class ListenMenuOpen implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+
+			JFileChooser chooser = new JFileChooser();
+
+			chooser.showOpenDialog(chooser);
+			File file = chooser.getSelectedFile();
+			String filename = file.getAbsolutePath();
+
 		}
 	}
 
@@ -73,7 +97,7 @@ public class OutbreakGui {
 		// Display Frame
 		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Frame.setSize(400, 500);
-		// Frame.setResizable(false);
+//		Frame.setResizable(false);
 		Frame.pack(); // size frame
 		Frame.setVisible(true);
 
@@ -90,21 +114,10 @@ public class OutbreakGui {
 		}
 	}
 
-	protected JComponent makePanel(String text) {
-		JPanel panel = new JPanel(false);
-		JLabel filler = new JLabel(text);
-		filler.setHorizontalAlignment(JLabel.CENTER);
-		panel.setLayout(new GridLayout(1, 1));
-		panel.add(filler);
-		return panel;
-	}
 
 	public static void main(String args[]) throws Exception {
 		OutbreakGui gui = new OutbreakGui();
 		gui.launchFrame();
-
-		// ReadNexus nex = new
-		// ReadNexus("/home/filip/Americas.104.YFV.E.GTRUCLN_BSSVS_dis.byregion_18_MCC.tre");
 
 	}
 
