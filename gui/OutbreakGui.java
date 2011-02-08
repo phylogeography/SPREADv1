30e1e26b7acd60fbe5886e0b57c1ae66c840f618
fbielejec <fbielejec@gmail.com>
2011-02-08 17:16:27 +0100

    simple gui

--- gui/OutbreakGui.java
+++ gui/OutbreakGui.java
@@ -1,8 +1,5 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -10,16 +7,17 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-import javax.swing.BorderFactory;
-import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 
@@ -28,7 +26,7 @@ public class OutbreakGui {
 	// Initialize all swing objects
 
 	// Frame
-	private JFrame Frame = new JFrame("app"); // create Frame
+	private JFrame Frame = new JFrame("TestlabOutbreak"); // create Frame
 
 	// Buttons with options
 	private JButton Open = new JButton("Open");
@@ -39,10 +37,19 @@ public class OutbreakGui {
 	// Menubar
 	private JMenuBar mb = new JMenuBar();
 
+    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd",
+			Locale.US);
+	
 	// Text fields
 	JTextField coordinatesNameParser = new JTextField("location", 10);
 	JTextField HPDParser = new JTextField("95%", 5);
-
+	JTextField mrsdStringParser = new JTextField(formatter.format(calendar.getTime()), 10);
+	JComboBox eraParser;
+	JTextField numberOfIntervalsParser = new JTextField("100", 5);
+	JTextField maxAltMappingParser = new JTextField("5000000", 5);
+	JTextField kmlPathParser = new JTextField("/home/filip/Pulpit/output.kml", 15);
+	
 	/**
 	 * Constructor for the GUI
 	 * */
@@ -68,25 +75,37 @@ public class OutbreakGui {
 		content.setLayout(new GridLayout(0, 1));
 
 		JPanel panel1 = new JPanel();
-		panel1.setBorder(new javax.swing.border.TitledBorder("1"));
-		JTextField textField1 = new JTextField("1");
-		panel1.add(textField1);
+		panel1.setBorder(new javax.swing.border.TitledBorder("Coordinate attribute name:"));
+		panel1.add(coordinatesNameParser);
 		content.add(panel1);
 
-		
 		JPanel panel2 = new JPanel();
-		panel2.setBorder(new javax.swing.border.TitledBorder("2"));
-		JTextField textField2 = new JTextField("2");
-		panel2.add(textField2);
+		panel2.setBorder(new javax.swing.border.TitledBorder("HPD:"));
+		panel2.add(HPDParser);
 		content.add(panel2);
 
 		JPanel panel3 = new JPanel();
-		panel3.setBorder(new javax.swing.border.TitledBorder("3 / 4"));
-		JTextField textField3 = new JTextField("3");
-		JTextField textField4 = new JTextField("4");
-		panel3.add(textField3);
-		panel3.add(textField4);
+		panel3.setBorder(new javax.swing.border.TitledBorder("Most recent sampling date:"));
+		String era[] = {"AD","BC"};
+		eraParser = new JComboBox(era);
+		panel3.add(mrsdStringParser);
+		panel3.add(eraParser);
 		content.add(panel3);
+		
+		JPanel panel4 = new JPanel();
+		panel4.setBorder(new javax.swing.border.TitledBorder("number of intervals:"));
+		panel4.add(numberOfIntervalsParser);
+		content.add(panel4);
+
+		JPanel panel5 = new JPanel();
+		panel5.setBorder(new javax.swing.border.TitledBorder("maximal altitude:"));
+		panel5.add(maxAltMappingParser);
+		content.add(panel5);
+		
+		JPanel panel6 = new JPanel();
+		panel6.setBorder(new javax.swing.border.TitledBorder("kml name:"));
+		panel6.add(kmlPathParser);
+		content.add(panel6);
 		
 	}
 
