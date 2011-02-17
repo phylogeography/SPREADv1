2c8f3cddbc3f833c3c8b6e17e55fb0e40d1ee4fe
fbielejec <fbielejec@gmail.com>
2011-02-17 12:04:29 +0100

    future BF test to kml class

--- templates/RateIndicatorBF.java
+++ templates/RateIndicatorBF.java
@@ -5,13 +5,13 @@ import utils.Utils;
 
 public class RateIndicatorBF {
 
-	static ReadLocations data;
+	static ReadLocations locations;
 	static ReadLog indicators;
 
 	public static void main(String args[]) throws Exception {
 
 		// this will be parsed from gui:
-		data = new ReadLocations(
+		locations = new ReadLocations(
 				"/home/filip/Dropbox/Java-ML/JavaProjects/TestlabOutbreak/data/Philippe/locationDegrees"
 
 		);
@@ -21,16 +21,50 @@ public class RateIndicatorBF {
 
 		);
 
-//		Utils.Head2DArray(indicators.indicators, 2);
-//		Utils.Print2DArray(indicators.indicators);
-//		Utils.PrintArray(indicators.colNames);
-//		System.out.println(indicators.nrow);
-//		System.out.println(indicators.ncol);
-//		System.out.println(indicators.indicators[0].length);
+		int K = locations.nrow;
 
-		System.out.println("Done");
+		boolean symmetrical = false;
+		if (indicators.ncol == K * (K - 1)) {
+			symmetrical = false;
+		} else if (indicators.ncol == (K * (K - 1)) / 2) {
+			symmetrical = true;
+		} else {
+			System.err
+					.println("the number of rate indicators does not match the number of locations!");
+		}
+
+		// TODO: combinations
+
+		double qk = Double.NaN;
+		if (symmetrical) {
+			qk = (Math.log(2) + K - 1) / ((K * (K - 1)) / 2);
+		} else {
+			qk = (Math.log(2) + K - 1) / ((K * (K - 1)) / 1);
+		}
+
+		double[] pk = colMean(indicators.indicators);
+
+		Utils.PrintArray(pk);
+		
+		System.out.println(indicators.time);
 	}// END: main
-	
-	
 
+	private static double[] colMean(float[][] a) {
+
+		int nrow = a.length;
+		int ncol = a[0].length;
+		double sum;
+
+		double[] b = new double[ncol];
+		for (int col = 0; col < ncol; col++) {
+			sum = 0;
+			for (int row = 0; row < nrow; row++) {
+				sum += a[row][col];
+			}
+			b[col] = sum / ncol;
+		}
+		return b;
+	}
+
+	
 }// END: RateIndicatorBF
