4e4fce1779683d520370160fd2768a5262b98c7a
fbielejec <fbielejec@gmail.com>
2011-03-21 10:15:18 +0100

    RateIndicatorBFTab

--- src/gui/RateIndicatorBFTab.java
+++ src/gui/RateIndicatorBFTab.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.PrintStream;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -133,14 +134,16 @@ public class RateIndicatorBFTab extends JPanel {
 		JPanel panel9 = new JPanel();
 		textArea = new JTextArea(4, 20);
 		textArea.setEditable(true);
-		JScrollPane scrollPane = new JScrollPane(textArea);
+		JScrollPane scrollPane = new JScrollPane(textArea,
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		scrollPane.setPreferredSize(new Dimension(200, 70));
-		scrollPane
-				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane
-				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		panel9.add(scrollPane, BorderLayout.CENTER);
 		leftPanel.add(panel9);
+
+		// Redirect streams
+		System.setOut(new PrintStream(new JTextAreaOutputStream(textArea)));
+		System.setErr(new PrintStream(new JTextAreaOutputStream(textArea)));
 
 		JPanel leftPanelContainer = new JPanel();
 		leftPanelContainer.setLayout(new BorderLayout());
@@ -172,10 +175,10 @@ public class RateIndicatorBFTab extends JPanel {
 				chooser.showOpenDialog(chooser);
 				File file = chooser.getSelectedFile();
 				logFilename = file.getAbsolutePath();
-				textArea.setText("Opened " + logFilename + "\n");
+				textArea.append("Opened " + logFilename + "\n");
 
 			} catch (Exception e1) {
-				textArea.setText("Could not Open! \n");
+				textArea.append("Could not Open! \n");
 			}
 		}
 	}
@@ -191,10 +194,10 @@ public class RateIndicatorBFTab extends JPanel {
 				chooser.showOpenDialog(chooser);
 				File file = chooser.getSelectedFile();
 				locationsFilename = file.getAbsolutePath();
-				textArea.setText("Opened " + locationsFilename + "\n");
+				textArea.append("Opened " + locationsFilename + "\n");
 
 			} catch (Exception e1) {
-				textArea.setText("Could not Open! \n");
+				textArea.append("Could not Open! \n");
 			}
 		}
 	}
@@ -239,7 +242,7 @@ public class RateIndicatorBFTab extends JPanel {
 
 					} catch (Exception e) {
 						e.printStackTrace();
-						textArea.setText("FUBAR \n");
+						textArea.append("FUBAR \n");
 					}
 
 					return null;
@@ -282,7 +285,7 @@ public class RateIndicatorBFTab extends JPanel {
 
 					} catch (Exception e) {
 						e.printStackTrace();
-						textArea.setText("FUBAR \n");
+						textArea.append("FUBAR \n");
 					}
 
 					return null;
@@ -316,7 +319,7 @@ public class RateIndicatorBFTab extends JPanel {
 				textArea.setText("Saved " + plotToSaveFilename + "\n");
 
 			} catch (Exception e0) {
-				textArea.setText("Could not save! \n");
+				textArea.append("Could not save! \n");
 			}
 
 		}// END: actionPerformed
@@ -327,13 +330,17 @@ public class RateIndicatorBFTab extends JPanel {
 		if (imgURL != null) {
 			return new ImageIcon(imgURL);
 		} else {
-			textArea.setText("Couldn't find file: " + path + "\n");
+			textArea.append("Couldn't find file: " + path + "\n");
 			return null;
 		}
 	}
 
 	public void setText(String text) {
-		textArea.setText(text);
+		textArea.append(text);
+	}
+
+	public void clearTerminal() {
+		textArea.setText("");
 	}
 
 }
