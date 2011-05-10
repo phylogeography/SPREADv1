95755b7389aeac72cf6ed30e90b228367a7d13ea
fbielejec <fbielejec@gmail.com>
2011-05-10 13:32:55 +0200

    moved map background functionalisties to a separate class

---
+++ src/templates/MapBackground.java
@@ -0,0 +1,37 @@
+package templates;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+import utils.Setter;
+
+public class MapBackground {
+
+	private PApplet parent;
+	private PImage mapImage;
+	private String imgPath;
+
+	boolean fromJar = false;
+
+	public MapBackground(PApplet p) {
+
+		parent = p;
+
+		if (fromJar) {
+			imgPath = "jar:"
+					+ this.getClass().getResource("world_map.png").getPath();
+		} else {
+			imgPath = this.getClass().getResource("world_map.png").getPath();
+		}
+
+		// World map in Equirectangular projection
+		mapImage = parent.loadImage(imgPath);
+
+	}// END: MapBackground(PApplet p)
+
+	public void drawMapBackground() {
+
+		parent.image(mapImage, 0, 0, parent.width, parent.height);
+
+	}// END: drawMapBackground
+
+}// END: MapBackground class
