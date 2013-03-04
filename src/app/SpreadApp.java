edd6c376c9aebe7cb8aee84ca565d3a0f035bff3
filip <fbielejec@gmail.com>
2013-03-04 11:24:28 +0100

    new release

--- src/app/SpreadApp.java
+++ src/app/SpreadApp.java
@@ -39,7 +39,7 @@ public class SpreadApp {
     /**
      * Version string: assumed to be in format x.x.x
      */
-    private static final String VERSION = "1.0.6rc";
+    private static final String VERSION = "1.0.6";
     private static final String DATE_STRING = "2013";
 
 	// Dimension
